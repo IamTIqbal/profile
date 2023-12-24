095851ff8a0c4b4c42927c413cbc64d494e46b93
Tamim Iqbal <108043975+IamTIqbal@users.noreply.github.com>
2023-12-24 18:42:58 +0600

    changed

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+const form = document.querySelector("contactForm");
+const fullName = document.getElementById("name");
+const emailAddress = document.getElementById("email");
+const phoneNumber = document.getElementById("phone");
+const textMessage = document.getElementById("message");
+
+function sendEmail(){
+    const bodyMessage = `Full Name: ${fullName.value} <br>Email: ${emailAddress.value} <br>Phone Number: ${phoneNumber.value} <br>Message: ${textMessage.value}`;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "email@tamimiqbal.com",
+        Password : "281DBC5AB4284FFDDC5416BA0B0EF0AEEB74",
+        To : 'info.mtamim@gmail.com',
+        From : "email@tamimiqbal.com",
+        Subject : "Email From Website",
+        Body : bodyMessage
+    }).then(
+      message => alert(message)
+    );
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    sendEmail();
+})
